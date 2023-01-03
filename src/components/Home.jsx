cde874573990a7e5756ebad05713c8eb4adc2dcb
pooriavakili09 <pooriavakili09@gmail.com>
2023-01-04 00:56:21 +0330

    updateloadinf

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,59 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addTocart } from "../redux/features/cart";
+import "../assets/app.css"
 const Home = () => {
     const dispatch = useDispatch()
     const data = useSelector((state) => state.products.items)
-    return (<div className="grid 2xl:grid-cols-3 xl:grid-cols-2 grid-cols-1 place-items-center gap-6 ">
-        {
-            data.map((product) => (
-                <div key={product.id} className="card w-96 bg-base-100 shadow-xl">
-                    <figure><img className="" src={product.img} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">{product.name}</h2>
-                        <div className="card-actions justify-between">
-                            <div>{product.price}</div>
-                            <button onClick={() => dispatch(addTocart(product))} className="btn btn-primary">addTocart</button>
+    const [loading, setloading] = useState(false)
+    useEffect(() => {
+        setloading(true)
+        setTimeout(() => {
+
+            setloading(false)
+        }, 2000)
+    }, [])
+    return (
+        <div>
+            <div>
+
+                {
+                    loading ? (
+                        <div className="loader-container">
+                            <div className="spinner"></div>
                         </div>
-                    </div>
-                </div>
-            ))
-        }
-    </div>);
+
+                    ) : (
+
+                        <div className="grid 2xl:grid-cols-3 xl:grid-cols-2 grid-cols-1 place-items-center gap-6 ">
+
+                            {
+                                data.map((product) => (
+                                    <div key={product.id} >
+                                        <div className="card w-96 bg-base-100 shadow-xl">
+                                            <figure><img className="" src={product.img} alt="Shoes" /></figure>
+                                            <div className="card-body">
+                                                <h2 className="card-title">{product.name}</h2>
+                                                <div className="card-actions justify-between">
+                                                    <div>{product.price}</div>
+                                                    <button onClick={() => dispatch(addTocart(product))} className="btn btn-primary">addTocart</button>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+
+
+                                    </div >
+                                ))
+                            }
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+
+    )
 }
 
 export default Home;
