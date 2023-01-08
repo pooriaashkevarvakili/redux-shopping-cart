05a69dc215c820717b4dd3133e3c53015ab8ae45
pooriavakili09 <pooriavakili09@gmail.com>
2023-01-08 11:42:23 +0330

    404

--- front-end/src/components/NotFound.jsx
+++ front-end/src/components/NotFound.jsx
@@ -1,10 +1,13 @@
-import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
 
 const NotFound = () => {
-    return (<Fragment className="flex items-center justify-center">
-        <div>404</div>
+    return (<div className="flex flex-col w-full h-screen items-center justify-center">
+        <img src="404.png" alt="" />
+
         <div>این صفحه موجود نیست</div>
-    </Fragment>);
+        <Link className="btn btn-black text-white" to="/">Go to Home</Link>
+    </div>);
 }
 
 export default NotFound;
